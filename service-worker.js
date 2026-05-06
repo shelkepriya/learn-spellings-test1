c7ce0d7fe921caccfb1b035ea93a2eff3b79663b
shelkepriya <shelkepriya@gmail.com>
2026-05-06 20:17:31 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,6 @@
+self.addEventListener("install",e=>{
+ e.waitUntil(caches.open("app").then(c=>c.addAll(["index.html","quiz.html","parent.html"])));
+});
+self.addEventListener("fetch",e=>{
+ e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
